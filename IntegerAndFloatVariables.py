723e7d8b3c904fc5864f0f30a85b3506b294e9ea
Dave A. Omandam <76654820+Davie704@users.noreply.github.com>
2021-01-04 21:05:13 +0800

    Integer and Float Variables
    Today I learned about Integer and Float Variables.

---
+++ IntegerAndFloatVariables.py
@@ -0,0 +1,27 @@
+# Integers and Float Variables
+
+numeric_variable = 10
+print("Integer variable value is: " + str(numeric_variable))
+
+# Define simple variables
+
+simple_variable_1 = 5
+simple_variable_2 = 10
+
+result = simple_variable_1 + simple_variable_2
+print("Result: " + str(result))
+
+# Power 2 numbers
+power_result = simple_variable_1 ** 2
+print("Square root is: " + str(power_result))
+
+# Modulo of 2 numbers
+modulo_result = simple_variable_1 % 3
+print("Modulo result: " + str(modulo_result))
+
+# Decimal point variable -> Float
+decimal_var_1 = 2.5
+decimal_var_2 = 0.25
+
+summary_of_two_float = decimal_var_1 + decimal_var_2
+print("Summary of two float: " + str(summary_of_two_float))
