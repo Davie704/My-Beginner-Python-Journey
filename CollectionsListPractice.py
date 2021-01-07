2169cb3adce121806f75ffa65be3c4784749a927
Dave A. Omandam <76654820+Davie704@users.noreply.github.com>
2021-01-07 21:49:18 +0800

    Collections List Practice
    Today I learn basic knowledge about List

---
+++ CollectionsListPractice.py
@@ -0,0 +1,16 @@
+# Collections Type - List
+
+my_list = ["BMW", "Toyota", "Tesla", "Kia"]
+print("Cars List: " + str(my_list))
+print(my_list[2])
+print(my_list[1] == "Toyota")
+
+print(type(my_list))
+print(type(my_list[2]))
+
+mixed_values = ["Jim", 3500, "Alex", 2.53, True]
+print(mixed_values[:4])
+# print(mixed_values[6])
+
+my_list.append("Suzuki")
+print(my_list)
