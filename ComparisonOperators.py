f77df3b1bfde52aa14257485eae33466e8f5d139
Dave A. Omandam <76654820+Davie704@users.noreply.github.com>
2021-01-07 18:56:00 +0800

    Comparison Operators
    Basic understanding of Comparison Operators

---
+++ ComparisonOperators.py
@@ -0,0 +1,33 @@
+# Comparison Operators
+
+my_age = 30
+my_sisters_age = 30
+mothers_age = 60
+my_name = "Johnny"
+phone_name = "OnePlus 5t"
+friend_name = "Johnny"
+
+if my_sisters_age > my_age:
+    print("True")
+else:
+    print("False")
+if my_sisters_age != my_age:
+    print("True")
+else:
+    print("False")
+if my_sisters_age == my_age:
+    print("True")
+else:
+    print("False")
+if my_name == phone_name:
+    print("True")
+else:
+    print("False")
+if my_name != phone_name:
+    print("True")
+else:
+    print("False")
+if friend_name == my_name:
+    print("True")
+else:
+    print("False")
