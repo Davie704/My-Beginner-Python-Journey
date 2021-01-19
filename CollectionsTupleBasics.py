dca9d7bb64ac56e49233024acfb996806a004d73
Dave A. Omandam <76654820+Davie704@users.noreply.github.com>
2021-01-19 22:48:13 +0800

    Python Tuples
    Today I learn the basics of Tuples Collection.

---
+++ CollectionsTupleBasics.py
@@ -0,0 +1,11 @@
+# Collections Tuple Basics
+
+professions = ("Front end", "Back end", "DevOps", "QA")
+
+print("Professions in the industry: " + str(professions))
+print(type(professions))
+
+print("Print cell # 1: " + str(professions[1]))
+print("Print cell in the last collection: " + str(professions[-1]))
+print("Print range of cell in the collection: " + str(professions[1:3]))
+print("Length of the collection: " + str(len(professions)))
